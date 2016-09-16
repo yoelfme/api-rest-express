06e65e0a33aedd62d70d463b34097dd70e014461
Yoel Monzon <yoelfme@hotmail.com>
2016-09-16 02:44:52 -0600

    [add] route hello with param :name

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/hello/:name', (req, res) => {
+  res.send({ message: `Hello ${req.params.name}`})
+})
+
 app.listen(port, () => {
   console.log(`API REST running on http://localhost:${port}`)
 })
