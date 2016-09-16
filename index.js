6978de68ba1bfe4493b2a2ebae07ee80889ab603
Yoel Monzon <yoelfme@hotmail.com>
2016-09-16 02:44:16 -0600

    [add] body paser middleware and configure nodemon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-  console.log('API REST running on http://localhost:3000')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`API REST running on http://localhost:${port}`)
 })
